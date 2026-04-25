4f760cc246427d828e6a7489d4e18156c8bc33bc
PRABHAV <prabavyogpav@gmail.com>
2026-04-26 02:49:14 +0530

    bonus feature- Added skeleton loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,18 +1,57 @@
 import React from 'react';
-import {View,ActivityIndicator,StyleSheet} from 'react-native';
+import {View,StyleSheet} from 'react-native';
 
 export default function Loader(){
- return(
-   <View style={styles.container}>
-      <ActivityIndicator size="large"/>
-   </View>
- );
+
+return(
+<View style={styles.container}>
+
+<View style={styles.card}>
+<View style={styles.lineLarge}/>
+<View style={styles.lineMedium}/>
+<View style={styles.lineSmall}/>
+</View>
+
+<View style={styles.card}>
+<View style={styles.lineLarge}/>
+<View style={styles.lineMedium}/>
+<View style={styles.lineSmall}/>
+</View>
+
+<View style={styles.card}>
+<View style={styles.lineLarge}/>
+<View style={styles.lineMedium}/>
+<View style={styles.lineSmall}/>
+</View>
+
+</View>
+);
+
 }
 
 const styles=StyleSheet.create({
 container:{
-flex:1,
-justifyContent:'center',
-alignItems:'center'
+padding:16
+},
+card:{
+padding:16,
+marginBottom:12,
+borderWidth:1,
+borderRadius:8
+},
+lineLarge:{
+height:18,
+marginBottom:10,
+backgroundColor:'#ddd'
+},
+lineMedium:{
+height:14,
+marginBottom:10,
+backgroundColor:'#e5e5e5'
+},
+lineSmall:{
+height:14,
+width:'80%',
+backgroundColor:'#eee'
 }
 });
